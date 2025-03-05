bc775f251a74bbc9f47180e2d9b254119b5e66d4
Ranzz-source <sansoriginl@gmail.com>
2025-03-05 12:51:14 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Sans Bot",
+  botOwner: 'Sans', // Your name
+  ownerNumber: '85816306853', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
